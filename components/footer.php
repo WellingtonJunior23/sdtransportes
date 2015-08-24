32467b38c863604b71952fbe14c87c1b4f01e261
Wellington Junior <junior.mac91@hotmail.com>
2015-08-24 14:14:22 -0300

    Renomeado arquivos, e refatorando as paginas

---
+++ components/footer.php
@@ -0,0 +1,29 @@
+    <div class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <span>
+                        Copyright &copy; <?php echo date(Y);?>, S D Transportes
+                    </span>
+
+                </div> 
+                <div class="col-md-6 col-sm-6">
+                    <ul class="social">
+                        <li><a href="#" class="fa fa-facebook" title="Facebook"></a></li>
+                        <li><a href="#" class="fa fa-twitter" title="Twitter"></a></li>                        
+                        <li><a href="#" class="fa fa-linkedin" title="Linkedin"></a></li>
+                    </ul>
+                </div> 
+            </div> 
+        </div> 
+    </div> 
+    
+    <script src="js/jquery-1.10.1.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+    <script src="js/jquery.easing-1.3.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>    
+</body>
+</html>
+
---
+++ components/topo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">    
+    <title>S D Transportes</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/misc.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/modernizr-2.6.2.min.js"></script>
+</head>
+
+<body>
